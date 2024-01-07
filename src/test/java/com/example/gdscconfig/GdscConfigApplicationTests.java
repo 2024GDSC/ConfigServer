b309784032b6e43282e25c52f7a98b60ea288079
반영환 <lopahn2@gmail.com>
2024-01-07 21:22:06 +0900

    feat: init config server

---
+++ src/test/java/com/example/gdscconfig/GdscConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gdscconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdscConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
